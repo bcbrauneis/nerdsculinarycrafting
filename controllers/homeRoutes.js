08eecfbf01eea4b180048b54fc3a704a6830459c
Melissa Bentley <melivesel@gmail.com>
2024-07-13 18:37:29 -0500

    created models for user and recipe

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,25 +4,25 @@ const withAuth = require('../utils/auth');
 
 
 
-  //router.get('/', async (req, res) => {
-   // try {
-      // Get all projects and JOIN with user data
-   //   const recipeData = await Recipe.findAll({
+  router.get('/', async (req, res) => {
+   try {
+  
+      const recipeData = await Recipe.findAll({
     
-  //    });
+     });
   
-      // Serialize data so the template can read it
-  //    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+       //Serialize data so the template can read it
+      const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
   
-      // Pass serialized data and session flag into template
-  //    res.render('homepage', { 
-  //      recipes, 
-    //    logged_in: req.session.logged_in 
-   //   });
-   /// } catch (err) {
-   //   res.status(500).json(err);
-   // }
-  //});
+      //Pass serialized data and session flag into template
+      res.render('homepage', { 
+       recipes, 
+       logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+     res.status(500).json(err);
+    }
+  });
   
   
 
